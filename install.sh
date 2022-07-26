069cbe0bbc6c48fdde27362a743be12491c30627
Bryan Helmkamp <bryan@brynary.com>
2022-07-26 15:51:07 -0400

    Updates

--- install.sh
+++ install.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# Via https://github.com/bstollnitz/dotfiles/blob/main/install.sh
 
-create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
+dotfiles_dir=$(pwd)
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+rm -f ~/.gitconfig
+ln -s $dotfiles_dir/gitconfig ~/.gitconfig
 
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
-}
+rm -f ~/.gitignore
+ln -s $dotfiles_dir/gitignore ~/.gitignore
 
-create_symlinks
+rm -f ~/.zshrc
+ln -s $dotfiles_dir/zshrc ~/.zshrc
 
 echo "Initializing conda for zsh."
 conda init zsh
--- zshrc
+++ zshrc
@@ -38,3 +38,19 @@ alias gam="git commit -v --amend"
 alias grb="git rebase -i HEAD~10"
 alias gca="git commit -a -v"
 alias gitrmall='git ls-files --deleted | xargs git rm'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/usr/local/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
